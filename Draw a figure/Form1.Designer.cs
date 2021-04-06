1ce36803d37a4c3d3f155ea1aa4e597056cea666
Mehrac Lətifli <meracletifli@outlook.com>
2021-04-06 14:08:06 +0400

    Add project files.

---
+++ Draw a figure/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Draw_a_figure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.heightTxtb = new System.Windows.Forms.TextBox();
+            this.widthTxtb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.ForeColor = System.Drawing.Color.Black;
+            this.radioButton2.Location = new System.Drawing.Point(76, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 24);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "EMPTY";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.ForeColor = System.Drawing.Color.Black;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 24);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FILL";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(371, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Color";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(449, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 28);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // heightTxtb
+            // 
+            this.heightTxtb.Location = new System.Drawing.Point(237, 73);
+            this.heightTxtb.Name = "heightTxtb";
+            this.heightTxtb.Size = new System.Drawing.Size(100, 20);
+            this.heightTxtb.TabIndex = 12;
+            this.heightTxtb.Text = "100";
+            // 
+            // widthTxtb
+            // 
+            this.widthTxtb.Location = new System.Drawing.Point(237, 20);
+            this.widthTxtb.Name = "widthTxtb";
+            this.widthTxtb.Size = new System.Drawing.Size(100, 20);
+            this.widthTxtb.TabIndex = 11;
+            this.widthTxtb.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(158, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Height";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(158, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Width";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(562, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 85);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(356, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 45);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Type of Figure";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(12, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 28);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.Text = "Manual";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(681, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.heightTxtb);
+            this.Controls.Add(this.widthTxtb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Draw a figure";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox heightTxtb;
+        private System.Windows.Forms.TextBox widthTxtb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Draw a figure/Form1.cs
@@ -0,0 +1,565 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw_a_figure
+{
+    public partial class Form1 : Form
+    {
+        Point xy;
+        Point x2y2;
+
+        Pen pen = new Pen(Color.Red, 5);
+        SolidBrush brush = new SolidBrush(Color.Red);
+
+        Boolean ismousedown;
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<string> figures = new List<string> { "TriAngle", "Circle", "Rectangle" };
+            comboBox1.Items.AddRange(figures.ToArray());
+            comboBox1.SelectedIndex = 2;
+        }
+
+        IFactory FigureFactory { get; set; }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        
+        var item = comboBox1.SelectedItem.ToString();
+            if (item == "TriAngle")
+            {
+                FigureFactory = new TriAngleFactory();
+            }
+            else if (item == "Rectangle")
+            {
+                FigureFactory = new RectangleFactory();
+            }
+            else if (item == "Circle")
+            {
+                FigureFactory = new CircleFactory();
+            }
+        }
+
+        List<IFigure> Figures = new List<IFigure>();
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                if (radioButton1.Checked)
+                {
+                    if (FigureFactory.GetFigure() is Rectangle rect)
+                    {
+
+
+                        rect.Color = FigureColor;
+                        rect.Point = e.Location;
+                        rect.Size = new Size(int.Parse(widthTxtb.Text), int.Parse(heightTxtb.Text));
+
+
+                        rect.isFilled = true;
+
+                        Figures.Add(rect);
+                    }
+
+                    if (FigureFactory.GetFigure() is Circle circle)
+                    {
+
+
+                        circle.Color = FigureColor;
+                        circle.Point = e.Location;
+                        circle.Size = new Size(int.Parse(widthTxtb.Text), int.Parse(heightTxtb.Text));
+
+
+                        circle.isFilled = true;
+
+                        Figures.Add(circle);
+                    }
+
+                    if (FigureFactory.GetFigure() is TriAngle triAngle)
+                    {
+
+
+                        triAngle.Color = FigureColor;
+                        triAngle.Point = e.Location;
+                        triAngle.Size = new Size(int.Parse(widthTxtb.Text), int.Parse(heightTxtb.Text));
+
+
+                        triAngle.isFilled = true;
+
+                        Figures.Add(triAngle);
+                    }
+
+                    this.Refresh();
+                }
+
+
+
+
+                if (radioButton2.Checked)
+                {
+                    if (FigureFactory.GetFigure() is Rectangle rect)
+                    {
+
+
+                        rect.Color = FigureColor;
+                        rect.Point = e.Location;
+                        rect.Size = new Size(int.Parse(widthTxtb.Text), int.Parse(heightTxtb.Text));
+
+
+                        rect.isFilled = false;
+
+                        Figures.Add(rect);
+                    }
+
+                    if (FigureFactory.GetFigure() is Circle circle)
+                    {
+
+
+                        circle.Color = FigureColor;
+                        circle.Point = e.Location;
+                        circle.Size = new Size(int.Parse(widthTxtb.Text), int.Parse(heightTxtb.Text));
+
+
+                        circle.isFilled = false;
+
+                        Figures.Add(circle);
+                    }
+
+                    if (FigureFactory.GetFigure() is TriAngle triAngle)
+                    {
+
+
+                        triAngle.Color = FigureColor;
+                        triAngle.Point = e.Location;
+                        triAngle.Size = new Size(int.Parse(widthTxtb.Text), int.Parse(heightTxtb.Text));
+
+
+                        triAngle.isFilled = false;
+
+                        Figures.Add(triAngle);
+                    }
+
+                    this.Refresh();
+                }
+
+            }
+
+
+            if (checkBox1.Checked)
+            {
+               
+
+                
+
+
+                if (radioButton1.Checked)
+                {
+                    if (FigureFactory.GetFigure() is Rectangle rect)
+                    {
+
+
+                        rect.Color = FigureColor;
+                        rect.Point = e.Location;
+                        rect.Size = new Size(Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+
+
+                        rect.isFilled = true;
+
+                        Figures.Add(rect);
+                    }
+
+                    if (FigureFactory.GetFigure() is Circle circle)
+                    {
+
+
+                        circle.Color = FigureColor;
+                        circle.Point = e.Location;
+                        circle.Size = new Size(Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+
+
+                        circle.isFilled = true;
+
+                        Figures.Add(circle);
+                    }
+
+                    if (FigureFactory.GetFigure() is TriAngle triAngle)
+                    {
+
+
+                        triAngle.Color = FigureColor;
+                        triAngle.Point = e.Location;
+                        triAngle.Size = new Size(Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+
+
+                        triAngle.isFilled = true;
+
+                        Figures.Add(triAngle);
+                    }
+
+                    this.Refresh();
+                }
+
+
+                if (radioButton2.Checked)
+                {
+                    if (FigureFactory.GetFigure() is Rectangle rect)
+                    {
+
+
+                        rect.Color = FigureColor;
+                        rect.Point = e.Location;
+                        rect.Size = new Size(Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+
+
+                        rect.isFilled = false;
+
+                        Figures.Add(rect);
+                    }
+
+                    if (FigureFactory.GetFigure() is Circle circle)
+                    {
+
+
+                        circle.Color = FigureColor;
+                        circle.Point = e.Location;
+                        circle.Size = new Size(Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+
+
+                        circle.isFilled = false;
+
+                        Figures.Add(circle);
+                    }
+
+                    if (FigureFactory.GetFigure() is TriAngle triAngle)
+                    {
+
+
+                        triAngle.Color = FigureColor;
+                        triAngle.Point = e.Location;
+                        triAngle.Size = new Size(Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+
+
+                        triAngle.isFilled = false;
+
+                        Figures.Add(triAngle);
+                    }
+
+                    this.Refresh();
+                }
+
+            }
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+      
+            if (ismousedown == true)
+            {
+              
+                        x2y2 = e.Location;
+
+
+                this.Refresh();
+
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                x2y2 = e.Location;
+                ismousedown = false;
+
+
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            xy = e.Location;
+            ismousedown = true;
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                using (var a = e.Graphics)
+                {
+                    Pen pen = new Pen(FigureColor, 3);
+                    SolidBrush brush = new SolidBrush(FigureColor);
+
+
+                    foreach (var item in Figures)
+                    {
+                        if (item is Rectangle rect)
+                        {
+                            if (rect.isFilled)
+                            {
+                                a.FillRectangle(brush, rect.Point.X, rect.Point.Y, rect.Size.Width, rect.Size.Height);
+
+
+                            }
+
+                            else
+                            {
+                                a.DrawRectangle(pen, rect.Point.X, rect.Point.Y, rect.Size.Width, rect.Size.Height);
+                            }
+                        }
+
+                        if (item is Circle circle)
+                        {
+                            if (circle.isFilled)
+                            {
+                                a.FillEllipse(brush, circle.Point.X, circle.Point.Y, circle.Size.Width, circle.Size.Height);
+                            }
+
+                            else
+                            {
+                                a.DrawEllipse(pen, circle.Point.X, circle.Point.Y, circle.Size.Width, circle.Size.Height);
+                            }
+                        }
+
+
+                        if (item is TriAngle triAngle)
+
+                        {
+
+
+
+                            Point[] points = new Point[3]
+                             {
+
+
+                                           new Point(triAngle.Point.X + triAngle.Size.Width / 2, triAngle.Point.Y),
+                                           new Point(triAngle.Point.X + triAngle.Size.Width , triAngle.Point.Y+ triAngle.Size.Height),
+                                           new Point(triAngle.Point.X, triAngle.Point.Y+ triAngle.Size.Height),
+
+
+                             };
+
+                            if (triAngle.isFilled)
+                            {
+                                a.FillPolygon(brush, points);
+                            }
+
+                            else
+                            {
+                                a.DrawPolygon(pen, points);
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            if (checkBox1.Checked)
+            {
+                using (var a = e.Graphics)
+                {
+                    Pen pen = new Pen(FigureColor, 3);
+                    SolidBrush brush = new SolidBrush(FigureColor);
+
+
+                    foreach (var item in Figures)
+                    {
+                        if (item is Rectangle rect)
+                        {
+
+
+                            if (rect.isFilled)
+                            {
+                             
+                                   a.FillRectangle(brush, Math.Min(xy.X, x2y2.X), Math.Min(xy.Y, x2y2.Y), Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+                                
+                            }
+
+                            else
+                            {
+                              
+                                   a.DrawRectangle(pen, Math.Min(xy.X, x2y2.X), Math.Min(xy.Y, x2y2.Y), Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+                                
+
+                            }
+                        }
+
+                        if (item is Circle circle)
+                        {
+                            if (circle.isFilled)
+
+                            {
+                                a.FillEllipse(brush, Math.Min(xy.X, x2y2.X), Math.Min(xy.Y, x2y2.Y), Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+                            }
+
+                            else
+                            {
+                                a.DrawEllipse(pen, Math.Min(xy.X, x2y2.X), Math.Min(xy.Y, x2y2.Y), Math.Abs(x2y2.X - xy.X), Math.Abs(x2y2.Y - xy.Y));
+                            }
+                        }
+
+                        if (item is TriAngle triAngle)
+
+                        {
+
+                            Point[] points = new Point[3]
+                                {
+
+
+                                           new Point(Math.Min(xy.X, x2y2.X) + Math.Abs(x2y2.X - xy.X) / 2, Math.Min(xy.Y, x2y2.Y)),
+                                           new Point(Math.Min(xy.X, x2y2.X) + Math.Abs(x2y2.X - xy.X), Math.Min(xy.Y, x2y2.Y)+ Math.Abs(x2y2.Y - xy.Y)),
+                                           new Point(Math.Min(xy.X, x2y2.X),  Math.Min(xy.Y, x2y2.Y)+ Math.Abs(x2y2.Y - xy.Y)),
+
+
+                                };
+
+                            if (triAngle.isFilled)
+                            {
+                                a.FillPolygon(brush, points);
+                            }
+
+                            else
+                            {
+                                a.DrawPolygon(pen, points);
+
+                            }
+
+                        }
+                        
+                        
+                    }
+                }
+            }
+        }
+
+
+
+
+        Color FigureColor { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            var result = colorDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FigureColor = colorDialog.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Figures.Clear();
+            
+                    this.Refresh();
+            
+            this.Invalidate();
+
+
+
+
+
+        }
+
+
+    }
+
+
+
+    interface IFigure
+    {
+        Point Point { get; set; }
+        Size Size { get; set; }
+        Color Color { get; set; }
+
+        bool isFilled { get; set; }
+    }
+
+
+    class Rectangle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+
+        public bool isFilled { get; set; }
+    }
+
+
+
+    class Circle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+
+        public bool isFilled { get; set; }
+    }
+
+
+    class TriAngle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+
+        public bool isFilled { get; set; }
+    }
+
+
+    interface IFactory
+    {
+        IFigure GetFigure();
+    }
+
+    class CircleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new Circle();
+        }
+    }
+
+
+    class RectangleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new Rectangle();
+        }
+    }
+
+
+    class TriAngleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new TriAngle();
+        }
+    }
+
+
+
+
+
+
+
+
+}
